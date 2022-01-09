22dbd72e938684c8bd95e13aaf63c0a5271f4267
Robert Jäschke <jaeschke@l3s.de>
2022-01-09 12:18:16 +0100

    switched to PWM output

--- rotaryledtest.py
+++ rotaryledtest.py
@@ -3,7 +3,6 @@
 
 #
 # Demo for simple rotary encoder with LED on Raspberry Pi Pico.
-# (rotates through red, green, blue, yellow, magenta, cyan, white)
 #
 # Usage: Copy rotaryledtest.py to /media/rja/CIRCUITPY/code.py
 #
@@ -29,36 +28,76 @@
 
 import rotaryio
 import board
-import digitalio
+import pwmio
+import random
+#import adafruit_fancyled.adafruit_fancyled as fancy
 
-encoder = rotaryio.IncrementalEncoder(board.GP14, board.GP15)
+# configure wiring
+encoder = rotaryio.IncrementalEncoder(board.GP15, board.GP14)
+OFF = 2**16 - 1
+ON  = 0
 rgb = (
-    digitalio.DigitalInOut(board.GP21),
-    digitalio.DigitalInOut(board.GP20),
-    digitalio.DigitalInOut(board.GP19)
+    pwmio.PWMOut(board.GP21, frequency=5000, duty_cycle=OFF),
+    pwmio.PWMOut(board.GP20, frequency=5000, duty_cycle=OFF),
+    pwmio.PWMOut(board.GP19, frequency=5000, duty_cycle=OFF)
 )
+RED = 0
+GREEN = 1
+BLUE = 2
 
-for c in rgb:
-    c.direction = digitalio.Direction.OUTPUT
 
+def set_color(led, rgb):
+    """Set LED to rgb using some checks. colors in range 0..255"""
+    for i, c in enumerate(rgb):
+        val = (255 - min(255, abs(c))) * (2**8 + 1)
+        print("  ", c, "→", val)
+        led[i].duty_cycle = val
+
+
+# functions mapping encoder positions to LED colors
+
+# used by rgbcmyk
 states = [
-    (False, False, False),
-    (True, False, False),
-    (False, True, False),
-    (False, False, True),
-    (True, True, False),
-    (True, False, True),
-    (False, True, True),
-    (True, True, True)
+    (0, 0, 0), # off
+    (1, 0, 0), # red
+    (0, 1, 0), # green
+    (0, 0, 1), # blue
+    (1, 1, 0), # yellow
+    (1, 0, 1), # magenta
+    (0, 1, 1), # cyan
+    (1, 1, 1)  # white
 ]
 
 
+def ls_rgbcmyk(pos, brightness=255):
+    """Loop through red, green, blue, yellow, magenta, cyan, white"""
+    return [c*brightness for c in states[pos % len(states)]]
+
+
+def ls_saturation(pos, color=RED, steps=9):
+    """Loop through saturation of a random color"""
+    values = [0, 0, 0]
+    values[color] = int((pos % steps) * (255/(steps - 1)))
+    return values
+
+
+def ls_hue(pos):
+    """Rotate through hue"""
+    pass
+
+
+# functions to loop through
+funcs = [ls_rgbcmyk]
+funcs = [ls_saturation]
+
 last_position = None
+funci = 0
+
 while True:
     position = encoder.position
     if last_position is None or position != last_position:
-        state = states[position % len(states)]
-        print(position, state)
-        for i, c in enumerate(rgb):
-            c.value = state[i]
+        func = funcs[funci]
+        col = func(position)
+        print(position, col)
+        set_color(rgb, col)
     last_position = position

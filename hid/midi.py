dfedd664f86b64acebb87b280c862a1b218e4e8b
Robert Jäschke <jaeschke@l3s.de>
2023-01-17 19:56:42 +0100

    added rotary encoder as pitch bend controller :-)

--- hid/midi.py
+++ hid/midi.py
@@ -11,19 +11,23 @@
 # Relevant resources:
 # - https://blog.4dcu.be/diy/2021/05/20/MIDIpad.html
 #
+# TODO:
+# - use button debouncer? see multi.py or https://github.com/CrazyRobMiles/PICO-MIDI-Crackers-Controller/blob/main/code/CrackersController.py
+#
 # Changes:
 # 2023-01-17 (rja)
 # - initial version
 
 import board
 import digitalio
+import rotaryio
 import time
 import usb_midi
 import adafruit_midi
 
 from adafruit_midi.note_on import NoteOn
 from adafruit_midi.note_off import NoteOff
-# from adafruit_midi.pitch_bend import PitchBend
+from adafruit_midi.pitch_bend import PitchBend
 # from adafruit_midi.control_change import ControlChange
 
 note_mapping = [
@@ -56,9 +60,27 @@ for btn in buttons:
 pressed_buttons = [False for _ in button_pins]
 triggered_buttons = [False for _ in button_pins]
 
+# set up rotary encoder
+encoder = rotaryio.IncrementalEncoder(board.GP15, board.GP14)
+encoder_last_pos = 0
+
+bend = 8192 # range: 0 ... 16383 with 8192 no bend
+bend_step = 683
 
 # main loop
 while True:
+
+    # handle rotary encoder
+    encoder_pos = encoder.position
+    if encoder_pos != encoder_last_pos:
+        print("encoder:", encoder_pos, encoder_last_pos)
+        if encoder_pos > encoder_last_pos:
+            bend = min(16383, bend + bend_step)
+        elif encoder_pos < encoder_last_pos:
+            bend = max(0, bend - bend_step)
+        print("bending:", bend)
+        midi.send(PitchBend(bend))
+    encoder_last_pos = encoder_pos
 
     # set all pressed buttons to True
     for ix, btn in enumerate(buttons):

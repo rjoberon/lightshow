26b45bd6f5fa9da4b23e3a82cd8c981a18b4bdc3
Robert Jäschke <jaeschke@l3s.de>
2024-01-13 15:29:15 +0100

    added another rotary encoder and two switches

--- lightshow/neopixel.py
+++ lightshow/neopixel.py
@@ -11,24 +11,32 @@
 # Source: https://learn.adafruit.com/getting-started-with-raspberry-pi-pico-circuitpython/neopixel-leds
 #
 # Wiring:
-# | device  | pin    | function                  | Pico |
-# |---------+--------+---------------------------+------|
-# | LED     | black  | GND                       | GND  |
-# | strip   | red    | 5VDC                      | VBUS |
-# |         | yellow | DIN                       | GP0  |
-# |---------+--------+---------------------------+------|
-# | RGB     | A      | rotary encoder            | GP3  |
-# | rotary  | B      | rotary encoder            | GP4  |
-# | encoder | C      | rotary encoder GND        | GND  |
-# |         | 1      | LED red                   | -    |
-# |         | 2      | LED green                 | -    |
-# |         | 3      | switch                    | -    |
-# |         | 4      | LED blue                  | -    |
-# |         | 5      | common anode LED & switch | -    |
+# | device  | pin    | function                  | Pico    |
+# |---------+--------+---------------------------+---------|
+# | LED     | black  | GND                       | GND     |
+# | strip   | red    | 5VDC                      | VBUS    |
+# |         | yellow | DIN                       | GP0     |
+# |---------+--------+---------------------------+---------|
+# | RGB     | A      | rotary encoder            | GP3     |
+# | rotary  | B      | rotary encoder            | GP4     |
+# | encoder | C      | rotary encoder GND        | GND     |
+# |         | 1      | LED red                   | -       |
+# |         | 2      | LED green                 | -       |
+# |         | 3      | switch                    | GP2     |
+# |         | 4      | LED blue                  | -       |
+# |         | 5      | common anode LED & switch | 3.3V_EN |
+# |---------+--------+---------------------------+---------|
+# | EC11    | A      | rotary encoder            | GP10    |
+# | rotary  | B      | rotary encoder            | GP11    |
+# | encoder | C      | GND                       | GND     |
+# |         | D      | GND                       | GND     |
+# |         | E      | switch                    | GP12    |
 #
 # Author: rja
 #
 # Changes:
+# 2024-01-13 (rja)
+# - connected switches and 2nd rotary encoder
 # 2024-01-03 (rja)
 # - added selection of different effects using the rotary encoder
 # 2023-12-30 (rja)
@@ -41,12 +49,13 @@
 # Tasks:
 # - consider using interrupt-based handling of rotary encoder, for example,
 #   https://pypi.org/project/micropython-rotary-encoder/
-
 import time
 import board
 import neopixel
 import rotaryio
+import digitalio
 import demos
+from adafruit_debouncer import Debouncer
 
 
 # Update this to match the number of NeoPixel LEDs connected to your board.
@@ -54,13 +63,15 @@ num_pixels = 8
 gpio_neopixel = board.GP0
 
 # configure wiring
+
+# rotary encoder with switch and RGB LED
 # rotary encoder
-encoder = rotaryio.IncrementalEncoder(board.GP3, board.GP4)
+encoder1 = rotaryio.IncrementalEncoder(board.GP3, board.GP4)
 # switch
-#pin = digitalio.DigitalInOut(board.GP22)
-#pin.direction = digitalio.Direction.INPUT
-#pin.pull = digitalio.Pull.DOWN
-#switch = Debouncer(pin)
+pin1 = digitalio.DigitalInOut(board.GP2)
+pin1.direction = digitalio.Direction.INPUT
+pin1.pull = digitalio.Pull.DOWN
+switch1 = Debouncer(pin1)
 # LED
 #rgb = (
 #    pwmio.PWMOut(board.GP21, frequency=5000, duty_cycle=2**16 - 1),
@@ -68,7 +79,16 @@ encoder = rotaryio.IncrementalEncoder(board.GP3, board.GP4)
 #    pwmio.PWMOut(board.GP19, frequency=5000, duty_cycle=2**16 - 1)
 #)
 
+# rotary encoder with switch
+# rotary encoder
+encoder2 = rotaryio.IncrementalEncoder(board.GP11, board.GP10)
+# switch
+pin2 = digitalio.DigitalInOut(board.GP12)
+pin2.direction = digitalio.Direction.INPUT
+pin2.pull = digitalio.Pull.UP
+switch2 = Debouncer(pin2)
 
+# FIXME: move to demos.py
 effects = [
     demos.ls_position,
     demos.ls_unary,
@@ -87,23 +107,46 @@ with neopixel.NeoPixel(gpio_neopixel, num_pixels, auto_write=False) as pixels:
     pixels.brightness = 0.25
 
     k = 0                           # running variable for effect
-    last_position = None            # last position of rotary encoder
+    last_pos1 = None                # last position of rotary encoder 1
+    last_pos2 = None                # last position of rotary encoder 2
+
+    mode1 = 0                       # 0 = speed, 1 = step
+    speed = 4                       # wait time in 1/seconds (?)
 
     while True:
-        # handle rotary encoder
-        position = encoder.position
-        if last_position is None or position != last_position:
-            print(position)         # position changed → change effect
-            effect = effects[position % len(effects)]
-            k = 0
-        last_position = position
+        # handle rotary encoder1 (speed or stepping)
+        pos1 = encoder1.position
+        if last_pos1 is None or pos1 != last_pos1:
+            if mode1 == 0:
+                speed = pos1                      # change speed
+            else:
+                k = pos1                          # step through effect
+            print("pos1", pos1, speed, k)
+        last_pos1 = pos1
 
-        # handle effect
+        # handle switch1
+        switch1.update()
+        # handle switch of rotary encoder
+        if switch1.rose:
+            mode1 = (mode1 + 1) % 2
+            print("mode is now", mode1)
+
+        # handle rotary encoder2 (effect)
+        pos2 = encoder2.position
+        if last_pos2 is None or pos2 != last_pos2:
+            effect = effects[pos2 % len(effects)] # change effect
+            k = 0                                 # start effect at 0
+            print(effect)
+        last_pos2 = pos2
+
+
+        # show effect
         effect(k, num_pixels, pixels, demos.col_const)
         pixels.show()
-        if k > 255:
-            k = 0
-        else:
-            k += 1
 
-        time.sleep(0.05)
+        if mode1 == 0:                            # step automatically
+            k += 1
+        if k > 255:                               # reset
+            k = 0
+
+        time.sleep(1/2**speed)                       # wait
